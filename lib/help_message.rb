b2e6ac40f6baf5dfe523367dd70b28efe902965b
Will Mitchell <wvmitchell@gmail.com>
2013-09-17 11:10:47 -0600

    Created and tested new HelpMessage class

---
+++ lib/help_message.rb
@@ -0,0 +1,70 @@
+class HelpMessage
+
+  attr_reader :message_to_output
+
+  def initialize(code=nil)
+    @message_to_output = code.nil? ? HelpMessage.default_message : interpret_code(code.downcase)
+  end
+
+  def self.default_message
+    """
+      There are several commands available to you
+
+      load <filename> :
+        load a new file with the given <filename>
+
+      queue <count, clear, print> :
+        <count> : give the number of items in the queue
+        <clear> : empty the queue
+        <print> : print the queue to the screen
+
+      queue print by <attribute>
+        print the queue to the screen organized by <attribute>
+
+      queue save to <filename>
+        save the queue to the given <filename>
+
+      find <attribute> <criteria>
+        load the queue with all records matching the <criteria> for the given <attribute>
+    """
+  end
+
+  def interpret_code(code)
+    case code
+    when 'load' then load_message
+    when 'queue' then queue_message
+    when 'find' then find_message
+    else 'Sorry, but I can\'t help you with that'
+    end
+  end
+
+  def load_message
+    """
+      load <filename> :
+        load a new file with the given <filename>
+    """
+  end
+
+  def queue_message
+    """
+      queue <count, clear, print> :
+        <count> : give the number of items in the queue
+        <clear> : empty the queue
+        <print> : print the queue to the screen
+
+      queue print by <attribute>
+        print the queue to the screen organized by <attribute>
+
+      queue save to <filename>
+        save the queue to the given <filename>
+    """
+  end
+
+  def find_message
+    """
+      find <attribute> <criteria>
+        load the queue with all records matching the <criteria> for the given <attribute>
+    """
+  end
+
+end
---
+++ test/help_message_test.rb
@@ -0,0 +1,33 @@
+require 'minitest'
+require 'minitest/autorun'
+require './lib/help_message'
+
+class HelpMessageTest < MiniTest::Unit::TestCase
+
+  def test_default_to_generic_help_message
+    hm = HelpMessage.new
+    message = HelpMessage.default_message
+    assert_equal message, hm.message_to_output
+  end
+
+  def test_code_for_help_message_invalid
+    hm = HelpMessage.new('potato')
+    message = "Sorry, but I can't help you with that"
+    assert_equal message, hm.message_to_output
+  end
+
+  def test_code_executes_correct_function
+    ['load', 'queue', 'find'].each do |code|
+      hm = HelpMessage.new(code)
+      message = hm.send("#{code}_message")
+      assert_equal message, hm.message_to_output  
+    end
+  end
+
+  def test_uppercase_request_valid
+    hm = HelpMessage.new('LOAD')
+    message = hm.load_message
+    assert_equal message, hm.message_to_output
+  end
+
+end
